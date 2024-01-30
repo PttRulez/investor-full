578e0b139753f1e66d658b90672c3d30e292bcf1
satoshi <sashafem@mail.ru>
2024-01-30 11:06:18 +0300

    Testing GA

--- apps/client/src/constants/common.ts
+++ apps/client/src/constants/common.ts
@@ -1,7 +1,7 @@
 const urls = {
   fmp3: 'https://financialmodelingprep.com/api/v3',
   moex: 'https://iss.moex.com/iss',
-  investor: process.env.INVESTOR_API_URL,
+  investor: process.env.NEXT_PUBLIC_INVESTOR_API_URL,
 };
 
 export { urls };
